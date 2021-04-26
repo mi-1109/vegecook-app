d2dc20ca31ab951005453c0658c99d5818d4a9fc
mi-1109 <meisuzuki.sophia@gmail.com>
2021-04-26 06:55:31 +0000

    [ADD] devise & Userモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_26_062746) do
+ActiveRecord::Schema.define(version: 2021_04_26_065054) do
 
   create_table "chat_rooms", force: :cascade do |t|
     t.integer "user_id"
@@ -94,6 +94,19 @@ ActiveRecord::Schema.define(version: 2021_04_26_062746) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
