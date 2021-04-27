0cb7901d6796045f2e857e8bac2364919c2374bb
mi-1109 <meisuzuki.sophia@gmail.com>
2021-04-27 10:33:48 +0000

    [ADD] 会員の新規登録・ログイン機能＆画面作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :user
-      new_customer_session_path
+      new_user_session_path
     else resource_or_scope == :admin
       new_admin_session_path
     end
