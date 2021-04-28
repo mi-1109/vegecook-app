56dd4bf6d23a238acc0a9af9346efbe8185160cd
mi-1109 <meisuzuki.sophia@gmail.com>
2021-04-28 10:22:50 +0000

    [ADD] 会員編集画面 / [ADD] 会員情報編集機能（update)

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -4,8 +4,24 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "プロフィールを更新しました！"
+    else
+      render action: :edit
+    end
   end
 
   def quit_confirm
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :profile_image, :is_paid, :is_deleted, :veg_type, :introduction)
   end
 end
