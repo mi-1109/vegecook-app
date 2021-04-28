869a80145d2e0bf59ab4b42db3ee343a1857d4cc
mi-1109 <meisuzuki.sophia@gmail.com>
2021-04-28 10:49:36 +0000

    [ADD] Userモデルとsessionsコントローラーに、退会会員のログイン無効化機能を実装

--- app/controllers/public/devise/sessions_controller.rb
+++ app/controllers/public/devise/sessions_controller.rb
@@ -24,4 +24,22 @@ class Public::Devise::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  before_action :reject_user, only: [:create]
+
+  protected
+
+  # ======= 会員ステータスが退会の場合ログインを無効化 =======
+  def reject_user
+    @user = User.find_by(email: params[:user][:email].downcase)
+    if @user
+      if (@user.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == false))
+        flash[:alert] = "退会済みです"
+        redirect_to new_user_session_path
+      end
+    else
+      flash[:alert] = "必須項目を入力してください"
+    end
+  end
+
 end
