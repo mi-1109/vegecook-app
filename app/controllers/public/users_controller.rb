5920cf19b1ef949ac13041eec04a2b683db4eb12
mi-1109 <meisuzuki.sophia@gmail.com>
2021-04-29 08:59:50 +0000

    [ADD] relationshipsコントローラーにcreate/destroyアクションを定義 / [ADD] usersコントローラーに一覧表示のアクションを定義

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -28,6 +28,16 @@ class Public::UsersController < ApplicationController
     redirect_to root_path
   end
 
+  def follows
+    @user = User.find(params[:id])
+    @users = @user.followings
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers
+  end
+
   private
 
   def user_params
