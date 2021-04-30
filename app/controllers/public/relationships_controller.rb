29c62b08e770ae40c01cf87620ac3d7f76a33f57
mi-1109 <meisuzuki.sophia@gmail.com>
2021-04-30 02:43:01 +0000

    [ADD] フォロー・フォロワー一覧の非同期通信処理を追加”

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -3,13 +3,15 @@ class Public::RelationshipsController < ApplicationController
   def create
     follow = current_user.active_relationships.build(follower_id: params[:user_id])
     follow.save
-    redirect_to root_path
+    @following = User.find(params[:user_id])
+    @follower = User.find(params[:user_id])
   end
 
   def destroy
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
     follow.destroy
-    redirect_to root_path
+    @following = User.find(params[:user_id])
+    @follower = User.find(params[:user_id])
   end
 
 end
