155fe8be6d65e9ec57c434f0c88b348ec867748c
mi-1109 <meisuzuki.sophia@gmail.com>
2021-05-01 03:36:33 +0000

    [ADD] レシピ新規投稿・下書き保存機能

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -5,10 +5,36 @@ class Public::RecipesController < ApplicationController
 
   def new
     @post_recipe = PostRecipe.new
+
+    for i in 1..4 do
+      @post_recipe.ingredients.build
+    end
+
+    for i in 1..6 do
+      @post_recipe.procedures.build
+    end
+  end
+
+  def create
+    @recipe = PostRecipe.new(recipe_params)
+    if params[:post]
+      @recipe.save
+      flash[:notice] = "レシピを投稿しました！"
+      redirect_to user_path(current_user)
+    elsif params[:update]
+      @recipe.update(is_draft: true)
+      flash[:notice] = "レシピを下書きに保存しました！"
+      redirect_to user_path(current_user)
+    else
+      flash[:alert] = "お手数ですが、再度お試しください。"
+      render :edit
+    end
   end
 
   def show
     @recipe = Recipe.find(params[:id])
+    @ingredients = @recipe.ingredient.all
+    @procedures = @recipe.procedure.all
   end
 
   def edit
