7d1efaef9bd49d7d2333b5df20d448613642436e
mi-1109 <meisuzuki.sophia@gmail.com>
2021-05-06 14:26:55 +0000

    [ADD] 有料会員登録機能を追加

--- app/controllers/public/premiums_controller.rb
+++ app/controllers/public/premiums_controller.rb
@@ -1,7 +1,21 @@
 class Public::PremiumsController < ApplicationController
+  before_action :authenticate_user!
+
   def payment
+  end
+
+  def update
+    @user = current_user
+    if @user.is_paid == false
+      @user.update(is_paid: true)
+      redirect_to premiums_complete_path
+    else
+      flash[:error] = "お客様はすでにプレミアム・プランに加入されています。"
+      render :payment
+    end
   end
 
   def complete
   end
+
 end
