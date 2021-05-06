10672de9a9c5f2c0931911e23a21519cf96d7a4c
mi-1109 <meisuzuki.sophia@gmail.com>
2021-05-06 06:44:00 +0000

    [ADD] コメント投稿・削除を非同期通信化

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -5,13 +5,11 @@ class Public::CommentsController < ApplicationController
     @comment = current_user.comments.new(comment_params)
     @comment.post_recipe_id = @post_recipe.id
     @comment.save
-    redirect_to post_recipe_path(@post_recipe)
   end
 
   def destroy
     @post_recipe = PostRecipe.find(params[:post_recipe_id])
     Comment.find_by(id: params[:id], post_recipe_id: params[:post_recipe_id]).destroy
-    redirect_to root_path
   end
 
   private
