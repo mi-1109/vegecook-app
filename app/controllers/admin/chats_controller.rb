fcbe5c195d74ccf97550e228db2a8759aed5ae01
mi-1109 <meisuzuki.sophia@gmail.com>
2021-05-07 07:03:38 +0000

    [ADD] 管理者側チャット一覧作成

--- app/controllers/admin/chats_controller.rb
+++ app/controllers/admin/chats_controller.rb
@@ -1,6 +1,10 @@
 class Admin::ChatsController < ApplicationController
-  
+
+  def index
+    @chats = Chat.all.order(created_at: "DESC").page(params[:page]).per(10)
+  end
+
   def show
   end
-  
+
 end
