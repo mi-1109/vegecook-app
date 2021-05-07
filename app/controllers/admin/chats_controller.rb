201b51305dc3d3546aebdb9849813998bde911f8
mi-1109 <meisuzuki.sophia@gmail.com>
2021-05-07 08:17:21 +0000

    [ADD] 管理者側のチャット投稿機能作成

--- app/controllers/admin/chats_controller.rb
+++ app/controllers/admin/chats_controller.rb
@@ -5,6 +5,27 @@ class Admin::ChatsController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    user_chat_room = @user.chat_room
+    chat_room = nil
+    if user_chat_room.nil?
+      chat_room = ChatRoom.create(user_id: @user.id)
+    else
+      chat_room = user_chat_room
+    end
+    @chats = chat_room.chats
+    @chat = Chat.new(chat_room_id: chat_room.id)
   end
 
+  def create
+    @chat = current_admin.chats.new(chat_params)
+    @chat.save
+    redirect_to admin_chats_path
+  end
+
+  private
+
+  def chat_params
+    params.require(:chat).permit(:message, :chat_room_id)
+  end
 end
