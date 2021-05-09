80e308e9b1e979040edbb91ddbab5f0f7d115f16
mi-1109 <meisuzuki.sophia@gmail.com>
2021-05-09 04:10:40 +0000

    [ADD] 管理者側フォーム問い合わせ表示画面作成（一覧・詳細）

--- app/controllers/admin/form_inquiries_controller.rb
+++ app/controllers/admin/form_inquiries_controller.rb
@@ -1,7 +1,11 @@
 class Admin::FormInquiriesController < ApplicationController
+
   def index
+    @form_inquiries = FormInquiry.all.order(created_at: "DESC").page(params[:page]).per(10)
   end
 
   def show
+    @form_inquiry = FormInquiry.find(params[:id])
+    @user = User.find(@form_inquiry.user_id)
   end
 end
