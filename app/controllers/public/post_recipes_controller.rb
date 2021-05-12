b8e5821c10756ae4fad988ba2c9b013c9bebf510
mi-1109 <meisuzuki.sophia@gmail.com>
2021-05-12 12:59:20 +0000

    [UPDATE] レシピ削除のエラー処理を編集

--- app/controllers/public/post_recipes_controller.rb
+++ app/controllers/public/post_recipes_controller.rb
@@ -13,22 +13,19 @@ class Public::PostRecipesController < ApplicationController
     @post_recipe = PostRecipe.new(post_recipe_params)
     if params[:post]
       if @post_recipe.save
-        flash[:notice] = "レシピを投稿しました！"
-        redirect_to post_recipe_path(@post_recipe)
+        redirect_to post_recipe_path(@post_recipe), notice: "レシピを投稿しました！"
       else
-        flash[:alert] = "登録できませんでした。お手数ですが、入力内容をご確認のうえ再度お試しください"
         @post_recipe.new_form_instance
         logger.debug(@post_recipe.errors.messages)
-        render :new
+        render :new, alert: "登録できませんでした。お手数ですが、入力内容をご確認のうえ再度お試しください"
       end
     else
       if @post_recipe.update(is_draft: true)
-        flash[:notice] = "レシピを下書きに保存しました！"
-        redirect_to user_path(current_user)
+        flash[:notice] =
+        redirect_to user_path(current_user), notice: "レシピを下書きに保存しました！"
       else
-        flash[:alert] = "登録できませんでした。お手数ですが、入力内容をご確認のうえ再度お試しください"
         @post_recipe.new_form_instance
-        render :new
+        render :new, alert: "登録できませんでした。お手数ですが、入力内容をご確認のうえ再度お試しください"
       end
     end
   end
@@ -41,6 +38,7 @@ class Public::PostRecipesController < ApplicationController
 
   def edit
     @post_recipe = PostRecipe.find(params[:id])
+    @post_recipe.new_form_instance
   end
 
   def update
@@ -50,16 +48,18 @@ class Public::PostRecipesController < ApplicationController
     if @post_recipe.update(post_recipe_params)
       redirect_to post_recipe_path(@post_recipe.id), notice: "レシピを更新しました！"
     else
-      flash[:notice] = "更新できませんでした。お手数ですが、入力内容をご確認のうえ再度お試しください"
       @post_recipe.new_form_instance
-      render action: :edit
+      render :edit, alert: "更新できませんでした。お手数ですが、入力内容をご確認のうえ再度お試しください"
     end
   end
 
   def destroy
     @post_recipe = PostRecipe.find(params[:id])
-    @post_recipe.destroy
-    redirect_to root_path
+    if @post_recipe.destroy
+      redirect_to user_path(current_user), notice:"レシピを削除しました"
+    else
+      render :edit, alert:"削除できませんでした。お手数ですが、再度お試しください"
+    end
   end
 
   private
