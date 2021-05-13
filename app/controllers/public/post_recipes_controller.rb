aeb57c17f466a5d97880751824b8c233f14537f9
mi-1109 <meisuzuki.sophia@gmail.com>
2021-05-13 11:50:45 +0000

    [ADD] RecordNotFoundの例外処理を記述

--- app/controllers/public/post_recipes_controller.rb
+++ app/controllers/public/post_recipes_controller.rb
@@ -21,7 +21,7 @@ class Public::PostRecipesController < ApplicationController
         logger.debug(@post_recipe.errors.messages)
         render :new, alert: "登録できませんでした。お手数ですが、入力内容をご確認のうえ再度お試しください"
       end
-    # 下書きボタンを押下した場合  
+    # 下書きボタンを押下した場合
     else
       if @post_recipe.update(is_draft: true)
         redirect_to user_path(current_user), notice: "レシピを下書きに保存しました！"
