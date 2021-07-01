658f0de18afa904d5c3991ccc039c31fdc38e183
mi-1109 <meisuzuki.sophia@gmail.com>
2021-07-01 21:04:25 +0900

    [UPDATE] jsファイルをリファクタリング

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,7 @@
 //= require_tree .
 /*global $*/
 
+  // トップ画面のスライダー設定
   $(document).on('turbolinks:load',function(){
     $('.heading-slider').slick({
       dots: true,
@@ -61,8 +62,9 @@
     });
   });
 
-// 行挿入時のインデックス作成のためのグローバル変数
-var count = 10;
+  // 行挿入時のインデックス作成のためのグローバル変数
+  var count = 10;
+
   function addRow(table_id) {
     var table = document.getElementById(table_id)
     window.count += 1;
@@ -74,13 +76,13 @@ var count = 10;
     var cell2 = row.insertCell(-1);
     var cell3 = row.insertCell(-1);
 
-
     //つくり方の場合
     if (table_id == "procedure-table"){
       cell1.innerHTML = '<textarea name="post_recipe[procedures_attributes][' + window.count + '][body]" id="post_recipe_body" placeholder="手順を記入" class="p-2 " cols="35" rows="4"></textarea>';
       cell1.classList.add('pl-0');
       cell2.innerHTML = '<input type="button" value="削除" onclick="deleteRow(this)">';
       cell2.classList.add('align-middle');
+    //材料の場合
     } else {
       cell1.innerHTML = '<input type="text" name="post_recipe[ingredients_attributes][' + window.count + '][name]" id="post_recipe_name" placeholder="例）にんじん" size="14">';
       cell1.classList.add('py-1');
@@ -98,12 +100,13 @@ var count = 10;
     tr.parentNode.deleteRow(tr.sectionRowIndex);
   }
 
+  // ページトップへ戻るボタンの動き
   $(function() {
-  $('.move-top a').on('click',function(event){
-    $('body, html').animate({
-      scrollTop:0
-    }, 1000);
-    event.preventDefault();
+    $('.move-top a').on('click',function(event){
+      $('body, html').animate({
+        scrollTop:0
+      }, 1000);
+      event.preventDefault();
+    });
   });
-});
 
